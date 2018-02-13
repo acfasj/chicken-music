8ddc72550c1d62d85c5f7fe600d29547b8e8b38c
acfasj <acfasj@gmail>
2018-02-13 16:56:16 +0800

    add tabs

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="singer">
+    歌手
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,5 +5,29 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    // 推荐
+    {
+      path: '/recommend',
+      component: () => import('components/recommend/recommend')
+    },
+    // 歌手
+    {
+      path: '/singer',
+      component: () => import('components/singer/singer')
+    },
+    // 排行
+    {
+      path: '/rank',
+      component: () => import('components/rank/rank')
+    },
+    // 搜索
+    {
+      path: '/search',
+      component: () => import('components/search/search')
+    }
   ]
 })
