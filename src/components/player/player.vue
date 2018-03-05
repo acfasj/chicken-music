c9e4859f87bbe342dc01563abe90ca6074a1ec0d
acfasj <acfasj@gmail.com>
2018-03-05 21:19:52 +0800

    player collapse animation

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,68 +1,82 @@
 <template>
   <div class="player" v-show="playlist.length > 0">
-    <!-- 全屏的播放器 -->
-    <div class="normal-player" v-show="fullScreen">
-      <!-- 高斯模糊的背景 -->
-      <div class="background">
-        <img width="100%" height="100%" :src="currentSong.image">
-      </div>
-      <!-- 顶部 -->
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="icon-back"></i>
+    <transition
+      name="normal"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave"
+      @afterLeave="afterLeave"
+    >
+      <!-- 全屏的播放器 -->
+      <div class="normal-player" v-show="fullScreen">
+        <!-- 高斯模糊的背景 -->
+        <div class="background">
+          <img width="100%" height="100%" :src="currentSong.image">
         </div>
-          <h1 class="title" v-html="currentSong.name"></h1>
-          <h2 class="subtitle" v-html="currentSong.singer"></h2>
-      </div>
-      <!-- 旋转的唱片 -->
-      <div class="middle">
-        <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
-              <img class="image" :src="currentSong.image">
+        <!-- 顶部 -->
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+            <h1 class="title" v-html="currentSong.name"></h1>
+            <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <!-- 旋转的唱片 -->
+        <div class="middle">
+          <div class="middle-l">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd">
+                <img class="image" :src="currentSong.image">
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 底部的操作区 -->
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon icon-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon icon-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon icon-center">
+              <i class="icon-play"></i>
+            </div>
+            <div class="icon icon-right">
+              <i class="icon-next"></i>
+            </div>
+            <div class="icon icon-right">
+              <i class="icon icon-not-favorite"></i>
             </div>
           </div>
         </div>
       </div>
-      <!-- 底部的操作区 -->
-      <div class="bottom">
-        <div class="operators">
-          <div class="icon icon-left">
-            <i class="icon-sequence"></i>
-          </div>
-          <div class="icon icon-left">
-            <i class="icon-prev"></i>
-          </div>
-          <div class="icon icon-center">
-            <i class="icon-play"></i>
-          </div>
-          <div class="icon icon-right">
-            <i class="icon-next"></i>
-          </div>
-          <div class="icon icon-right">
-            <i class="icon icon-not-favorite"></i>
-          </div>
+    </transition>
+    <transition name="mini">
+      <!-- 底部的迷你播放器 -->
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img width="40" height="40" :src="currentSong.image">
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control">
+          <i class="icon-playlist"></i>
         </div>
       </div>
-    </div>
-    <!-- 底部的迷你播放器 -->
-    <div class="mini-player" v-show="!fullScreen" @click="open">
-      <div class="icon">
-        <img width="40" height="40" :src="currentSong.image">
-      </div>
-      <div class="text">
-        <h2 class="name" v-html="currentSong.name"></h2>
-        <p class="desc" v-html="currentSong.singer"></p>
-      </div>
-      <div class="control">
-        <i class="icon-playlist"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from 'common/js/dom'
+
+const transform = prefixStyle('transform')
 
 export default {
   computed: {
@@ -78,6 +92,65 @@ export default {
     },
     open () {
       this.setFullScreen(true)
+    },
+    enter (el, done) {
+      const { x, y, scale } = this._getPosAndScale()
+
+      let animation = {
+        0: {
+          transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0, 0, 0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0, 0, 0) scale(1)`
+        }
+      }
+
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+    },
+    afterEnter () {
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave (el, done) {
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const { x, y, scale } = this._getPosAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done)
+    },
+    afterLeave () {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    _getPosAndScale () {
+      const miniWidth = 40
+      const miniX = miniWidth / 2 + 20
+      const miniY = window.innerHeight - miniWidth / 2 - 10
+      const normalWidth = window.innerWidth / 2
+      const normalX = normalWidth
+      const normalY = normalWidth * 0.8 / 2 + 80
+      // 以上坐标以屏幕左上角为原点计算
+
+      const scale = miniWidth / normalWidth
+      const x = -(normalX - miniX) // cdWrapper是最终的状态， 其位置相当于原点， 所以是负的
+      const y = miniY - normalY
+
+      return {
+        scale,
+        x,
+        y
+      }
     },
     ...mapMutations({
       setFullScreen: 'SET_FULLSCREEN'
@@ -154,16 +227,15 @@ export default {
           .cd
             width: 100%
             height: 100%
-            box-sizing: border-box
-            border: 10px solid rgba(255, 255, 255, 0.1)
-            border-radius: 50%
             .image
               position: absolute
               left: 0
               top: 0
               width: 100%
               height: 100%
+              box-sizing: border-box
               border-radius: 50%
+              border: 10px solid rgba(255, 255, 255, 0.1)
     .bottom
       position: absolute
       bottom: 50px
@@ -185,6 +257,17 @@ export default {
             font-size: 40px
         .icon-right
           text-align: left
+    // 过渡
+    &.normal-enter-active, &.normal-leave-active
+      transition: all 0.4s
+      .top, .bottom
+        transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+    &.normal-enter, &.normal-leave-to
+      opacity: 0
+      .top
+        transform: translate3d(0, -100px, 0)
+      .bottom
+        transform: translate3d(0, 100px, 0)
   // 迷你播放器
   .mini-player
     display: flex
@@ -192,10 +275,14 @@ export default {
     position: fixed
     left: 0
     bottom: 0
-    z-index: 100
+    z-index: 180
     width: 100%
     height: 60px
     background: $color-highlight-background
+    &.mini-enter-active, &.mini-leave-active
+      transition: all 0.4s
+    &.mini-enter, &.mini-leave-to
+      opacity: 0
     .icon
       flex: 0 0 40px
       width: 40px
